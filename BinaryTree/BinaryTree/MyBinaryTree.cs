56528c12a33a91d38692c4255bf32054c78b83b8
MGeri <matt9583@optusnet.com.au>
2019-04-04 11:07:09 +1100

    Adding files

---
+++ BinaryTree/BinaryTree/MyBinaryTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        public TreeNode Root;
+
+        public BinaryTree()
+        {
+            this.Root = null;
+        }
+
+        public void Dump()
+        {
+            this.PrintTree(this.Root, 0);
+        }
+
+        private void PrintTree(TreeNode node, int role, string indent = "")
+        {
+            string prefix = "";
+            switch (role)
+            {
+                case 0: // root node
+                    prefix = "──  Root ";
+                    break;
+                case -1: // left node
+                    prefix = indent + "├──  left  ";
+                    break;
+                case 1: // right node
+                    prefix = indent + "└──  right ";
+                    break;
+                default:
+                    break;
+            }
+            if (role != 0)
+            {
+                Console.WriteLine(indent + "│");
+            }
+            if (node == null)
+            {
+                Console.WriteLine(prefix + " null");
+            }
+            else
+            {
+                Console.WriteLine(prefix + node.Data);
+                string padding = (role == -1) ? "│    " : "     ";
+
+                this.PrintTree(node.Left, -1, indent + padding);
+                this.PrintTree(node.Right, 1, indent + padding);
+            }
+            
+            //log.debug(prefix + nodeConnection + nodeName);
+            return;
+        }
+    }
+
+    // Node of tree
+    class TreeNode
+    {
+        public int Data;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode()
+        {
+            this.Data = 0;
+            this.Left = null;
+            this.Right = null;
+        }
+        public TreeNode(int value)
+        {
+            this.Data = value;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree myTree = new BinaryTree();
+
+            myTree.Root = new TreeNode(2);
+            myTree.Root.Left = new TreeNode(7);
+            myTree.Root.Left.Left = new TreeNode(2);
+            myTree.Root.Left.Right = new TreeNode(6);
+            myTree.Root.Left.Right.Left = new TreeNode(5);
+            myTree.Root.Left.Right.Right = new TreeNode(11);
+            myTree.Root.Right = new TreeNode(5);
+            myTree.Root.Right.Right = new TreeNode(9);
+            myTree.Root.Right.Right.Left = new TreeNode(4);
+
+            myTree.Dump();
+            Console.ReadKey();
+        }
+    }
+}
